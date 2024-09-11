2810e8d996e97037c437d5c86318424fdfbecc3c
Ryuga00x <kagamikrnl0@tutamail.com>
2024-09-11 20:56:34 +0100

    added some intrerface
    Added an interface help for the hole project

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use clap::Parser;
-use wuukoproxy::arg::{get_bind_data, Cli};
+use wuukoproxy::{Parser,
+                arg::{get_bind_data,
+                    Cli,
+                    }
+
+                }
+        ;
 fn main(){
     let arga = Cli::parse();
     let (listen_host,port,binded_adrr) = get_bind_data(&arga);
-    
+
 }
